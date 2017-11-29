90bf3f50fe07e736776897beb2720792a3e56b6c
Nicholas-Jason Roache <njrr325@gmail.com>
2017-11-29 18:58:51 -0500

    Update Main.lua
    the current projectile actually falls

--- Main.lua
+++ Main.lua
@@ -8,24 +8,25 @@ function love.load()
 	objects = {} 
 	--Ground object where the game will take place
 	objects.Ground = {}
-	objects.Ground.body = love.physics.newBody(world,650/2,650/2)
-	objects.Ground.shape = love.physics.newRectangleShape(650,50)
+	objects.Ground.body = love.physics.newBody(world,650/2,650/2,"static")
+	objects.Ground.shape = love.physics.newRectangleShape(650,250)
 	objects.Ground.fixture = love.physics.newFixture(objects.Ground.body,objects.Ground.shape)
 	--Create the Player object
 	objects.Player = {}
 	objects.Player.body = love.physics.newBody(world,650/2,650/2,"dynamic")
+	objects.Player.body:setLinearDamping(0.1)
 	--temporarily a circle until we have a finalized design
-	objects.Player.shape = love.physics.newCircleShape(0,-6,20)
+	objects.Player.shape = love.physics.newCircleShape(6,0,20)
 	objects.Player.fixture = love.physics.newFixture(objects.Player.body,objects.Player.shape)
 	--set the background color to a nice blue
 	love.graphics.setBackgroundColor(104, 136, 248) 
   	love.window.setMode(650, 650)
   	--Create Projectile object
   	objects.Projectile = {}
-  	objects.Projectile.body = love.physics.newBody(world,650/2,650/2,"dynamic")
-  	objects.Projectile.shape = love.physics.newRectangleShape(0,-24,20,20)
+  	objects.Projectile.body = love.physics.newBody(world,20,5,"dynamic")
+  	objects.Projectile.shape = love.physics.newRectangleShape(50,5,20,20,90)
   	objects.Projectile.fixture = love.physics.newFixture(objects.Projectile.body,objects.Projectile.shape)
-  	
+  	objects.Projectile.body:setGravityScale(.125)
   	end
 
 function love.update(dt)
@@ -39,10 +40,14 @@ function love.update(dt)
     objects.Player.body:applyForce(-50, 0)
   end
   --Destroys the player object if they happen to be in the same spot
-  if objects.Player.body:getY() == objects.Projectile.body:getY() then
-  		objects.Player.body:destroy()
-  		objects.Player.shape:destroy()
+  if objects.Player.body:getPosition() == objects.Projectile.body:getPosition() then
+  		objects.Player.fixture:destroy()
   end
+  --Destroys the projectile object when it hits the ground
+  if objects.Ground.body:getY() == objects.Projectile.body:getY() then
+  		objects.Projectile.fixture:destroy()
+  end
+
 end
 
 function love.draw()
