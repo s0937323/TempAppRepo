f605ea2cb813514e3f94a307caa3b9de2d4f3ff6
Nicholas-Jason Roache <njrr325@gmail.com>
2017-12-09 19:45:48 -0500

    Update Main.lua

--- Main.lua
+++ Main.lua
@@ -1,62 +1,61 @@
---Create world
+class = require 'middleclass'
+require 'player'
+require 'projectile'
+
 function love.load()
 	-- body
 	love.physics.setMeter(64)
-	world = love.physics.newWorld(0,9.81*64,true)
+	GAME_WORLD = love.physics.newWorld(0, 9.81 * 64, true)
 
 	--Table containing all the objects
 	objects = {} 
 	--Ground object where the game will take place
 	objects.Ground = {}
-	objects.Ground.body = love.physics.newBody(world,650/2,650/2,"static")
+	objects.Ground.body = love.physics.newBody(GAME_WORLD,650/2,650/2,"static")
 	objects.Ground.shape = love.physics.newRectangleShape(650,250)
 	objects.Ground.fixture = love.physics.newFixture(objects.Ground.body,objects.Ground.shape)
-	--Create the Player object
-	objects.Player = {}
-	objects.Player.body = love.physics.newBody(world,650/2,650/2,"dynamic")
-	objects.Player.body:setLinearDamping(0.1)
-	--temporarily a circle until we have a finalized design
-	objects.Player.shape = love.physics.newCircleShape(6,0,20)
-	objects.Player.fixture = love.physics.newFixture(objects.Player.body,objects.Player.shape)
+
 	--set the background color to a nice blue
 	love.graphics.setBackgroundColor(104, 136, 248) 
-  	love.window.setMode(650, 650)
-  	--Creates multiple projectiles
-  	objects.Projectile = {}
-  	function objects.Projectile:new()
-  		-- body
-  		x = love.math.random(50,550)
-  	objects.Projectile.body = love.physics.newBody(world,x,5,"dynamic")
-  	objects.Projectile.shape = love.physics.newRectangleShape(20,20)
-  	objects.Projectile.fixture = love.physics.newFixture(objects.Projectile.body,objects.Projectile.shape)
-  	objects.Projectile.body:setGravityScale(.125)
-  	end
-  	i = 1
-  	while( i <=2) do
-  	p = objects.Projectile:new()
-  	
-  	i = i + 1
-  	end
+
+	--Creates multiple projectiles
+	objects.Projectile = {}
+	function objects.Projectile:new()
+		-- body
+		x = love.math.random(50,550)
+		objects.Projectile.body = love.physics.newBody(GAME_WORLD,x,5,"dynamic")
+		objects.Projectile.shape = love.physics.newRectangleShape(20,20)
+		objects.Projectile.fixture = love.physics.newFixture(objects.Projectile.body,objects.Projectile.shape)
+		objects.Projectile.body:setGravityScale(.125)
+	end
+
+	i = 1
+	while(i <= 2) do
+		p = objects.Projectile:new()
+
+		i = i + 1
+	end
+
+	PLAYER = player:new(10, 10)
 end
 
 function love.update(dt)
-	-- body
-	world:update(dt)
+	dt = math.min(1 / 60, dt)
+	
+	GAME_WORLD:update(dt)
+	PLAYER:update(dt)
 
 	--here we are going to create some keyboard events
-  if love.keyboard.isDown("right") then --press the right arrow key to push the ball to the right
-    objects.Player.body:applyForce(50, 0)
-  elseif love.keyboard.isDown("left") then --press the left arrow key to push the ball to the left
-    objects.Player.body:applyForce(-50, 0)
-  end
-  --Destroys the player object if they happen to be in the same spot
-  if objects.Player.body:getPosition() == objects.Projectile.body:getPosition() then
-  		objects.Player.fixture:destroy()
-  end
-  --Destroys the projectile object when it hits the ground
-  if objects.Ground.body:getY() == objects.Projectile.body:getY() then
-  		objects.Projectile.fixture:destroy()
-  end
+
+	
+	--[[--Destroys the player object if they happen to be in the same spot
+	if objects.Player.body:getPosition() == objects.Projectile.body:getPosition() then
+		objects.Player.fixture:destroy()
+	end
+	--Destroys the projectile object when it hits the ground
+	if objects.Ground.body:getY() == objects.Projectile.body:getY() then
+		objects.Projectile.fixture:destroy()
+	end]]
 
 end
 
@@ -65,13 +64,27 @@ function love.draw()
 	--Draws the ground object
 	love.graphics.setColor(72,160,14)
 	love.graphics.polygon("fill", objects.Ground.body:getWorldPoints(objects.Ground.shape:getPoints()))
-	--Draws the player object
-	love.graphics.setColor(193,47,14)
-	love.graphics.circle("fill",objects.Player.body:getX(), objects.Player.body:getY(),objects.Player.shape:getRadius())
+
+	PLAYER:draw()
+
 	--Draws the projectile object
 	love.graphics.setColor(15,7,3)
 	love.graphics.rectangle("fill",objects.Projectile.body:getX(),objects.Projectile.body:getY(),20,20,90)
 	--Borders
 	love.graphics.setColor(0,0,0)
 	love.graphics.line(-3,-6,-3,-12)
+end
+
+function love.keypressed(key)
+	if key == "right" then --press the right arrow key to push the ball to the right
+		PLAYER:move(50, 0)
+	elseif key == "left" then --press the left arrow key to push the ball to the left
+		PLAYER:move(-50, 0)
+	end
+end
+
+function love.keyreleased(key)
+	if key == "right" or key == "left" then
+		PLAYER:move(0, 0)
+	end
 end
